d79ef301424a3eaf79be9c91be5b95fc24045138
Misha <bukhalo1999@mail.ru>
2016-12-22 17:55:18 +0200

    Configured main form. Added icon. Added sounds.

--- TextToSpeech/Program.cs
+++ TextToSpeech/Program.cs
@@ -16,7 +16,7 @@ namespace TextToSpeech
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new fmMain());
         }
     }
 }
---
+++ TextToSpeech/Speech.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace TextToSpeech
+{
+    class Speech
+    {
+    }
+}
